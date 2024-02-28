ee5ab9377187e6aa51e1d51c62278e1b01c22667
2_ruthless <105881276+DaRuthlessPWR@users.noreply.github.com>
2024-02-28 16:29:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -26,12 +26,12 @@ module.exports = {
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: "lavalink.techpoint.world", //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
-			password: "techpoint", //- The password of the lavalink server.
+			host: "eu-lavalink.lexnet.cc", //- The host name or IP of the lavalink server.
+			port: 443, // The port that lavalink is listening to. This must be a number!
+			password: "lexn3tl@val!nk", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
-			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
+			secure: True, //- Can be either true or false. Only use true if ssl is enabled!
 		},
 	],
 	embedColor: "#A020F0", //- Color of the embeds, hex supported
